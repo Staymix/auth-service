7a6b6bb6a49491a34677b8994877dbb6e3fba8b6
Staymix <staszubov199530@gmail.com>
2024-01-05 13:21:27 +0600

    Add project

---
+++ src/main/java/ru/zubov/auth/service/AuthUsersLoader.java
@@ -0,0 +1,31 @@
+package ru.zubov.auth.service;
+
+import ru.zubov.auth.model.UserEntity;
+import ru.zubov.auth.repository.AuthUserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class AuthUsersLoader implements CommandLineRunner {
+    private final AuthUserRepository repository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public void run(String... args) throws Exception {
+        repository.save(
+                UserEntity.builder().userId(1)
+                        .username("martin").password(passwordEncoder.encode("martin123"))
+                        .build()
+        );
+
+        repository.save(
+                UserEntity.builder().userId(2)
+                        .username("bob").password(passwordEncoder.encode("bob123"))
+                        .build()
+        );
+    }
+}
